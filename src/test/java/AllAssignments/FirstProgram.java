e6171f59713ea3969ac166a7ee3bb3aff157f121
“malinimurthy123git config --global user.name “malinimurthy123 <krmalini77@gmail.comgit config --global user.email krmalini77@gmail.com>
2021-02-24 15:23:33 -0800

    Moved the Assignments into AllAssignments Package Folder

---
+++ src/test/java/AllAssignments/FirstProgram.java
@@ -0,0 +1,9 @@
+package AllAssignments;
+
+public class FirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("First Program");
+    }
+}
